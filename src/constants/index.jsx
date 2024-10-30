8e5047be81a6674edb071272ec7eaa0e014d2fa5
Assem Salama <assem.salama@icloud.com>
2024-10-30 19:38:59 +0300

    info edits

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -130,68 +130,53 @@ export const SKILLS = [
 
 export const EXPERIENCES = [
   {
-    title: "Lead Frontend Developer",
-    company: "Innovative Tech Solutions",
-    duration: "July 2020 - Present",
+    title: "Software Engineer",
+    company: "Brightskies Inc.",
+    duration: "Januray 2023 - Present",
     description:
-      "As the Lead Frontend Developer, I spearheaded the development of advanced web applications using cutting-edge technologies like React, Redux, and TypeScript. I worked closely with cross-functional teams, including designers, product managers, and backend developers, to deliver seamless and high-performance user experiences.",
+      "Experienced in API development and backend optimization with SAP Commerce ERP, I enhanced transaction speeds and system performance for Pharmaoverseas and Mahle Smartbike Systems. My work includes innovative email preview integration and interactive e-bike web features, driving measurable improvements in engagement and efficiency. Additionally, I developed a high-accuracy e-bike troubleshooting chatbot with 95% intent detection."
   },
   {
-    title: "Frontend Engineer",
-    company: "Digital Creations",
-    duration: "February 2016 - June 2020",
+    title: "Software Engineering Intern",
+    company: "Encopedia Solutions",
+    duration: "July 2020 - September 2021",
     description:
-      "At Digital Creations, I focused on building highly interactive and responsive web interfaces using HTML, CSS, JavaScript, and modern libraries like React. I collaborated closely with UX/UI designers to implement design changes that enhanced user engagement and satisfaction. My role involved optimizing website performance, ensuring cross-browser compatibility, and implementing SEO best practices. ",
+      "Developed expertise in native Android Java for mobile app functionality enhancements, achieving substantial accuracy improvements in data recording. Skilled in Flutter for cross-platform app development, with a focus on creating engaging, user-centric applications that drive interaction and operational efficiency. Gained strong abilities in optimizing mobile user experiences and delivering impactful, user-focused solutions."
   },
   {
-    title: "Junior Web Developer",
-    company: "Bright Future Technologies",
-    duration: "August 2014 - January 2016",
+    title: "Software Engineering Intern",
+    company: "The Talent Tier",
+    duration: "June 2019 - September 2019",
     description:
-      "In my role as a Junior Web Developer, I assisted in the development and maintenance of various web applications. I gained hands-on experience in utilizing HTML, CSS, and JavaScript to create user-friendly interfaces. I actively participated in team meetings, contributed to project planning, and collaborated with senior developers to implement new features.",
+      "Crafted the organization's website employing HTML, CSS, JavaScript, and Bootstrap within 3 months, resulting in a visually engaging and intuitive online platform."
   },
 ];
 
 export const EDUCATION = [
   {
-    degree: "Master of Science in Computer Science",
-    institution: "Stanford University",
-    duration: "September 2012 - June 2014",
+    degree: "Bachelor of Computer and Communication Engineering",
+    institution: "Alexandria University",
+    duration: "September 2017 - September 2022",
     description:
-      "Specialized in Human-Computer Interaction and Software Engineering. Completed a thesis on enhancing user experience in web applications through advanced interactive techniques. Participated in various projects involving frontend development, algorithms, and data structures. Graduated with honors.",
-  },
-  {
-    degree: "Bachelor of Science in Information Technology",
-    institution: "University of California, Berkeley",
-    duration: "September 2008 - June 2012",
-    description:
-      "Focused on web development, programming languages, and database management. Actively involved in coding clubs and hackathons, where I developed several web applications using HTML, CSS, JavaScript, and PHP. Completed a senior project on developing an e-commerce platform. Graduated with a high GPA.",
+      "My university specialization in Software Engineering as a major, equipped me with a solid foundation in designing and building software solutions tailored for real-world challenges. This field fostered my skills across a broad spectrum, including data structures, algorithms, and software architecture, while emphasizing best practices in coding, security, and system design. Through hands-on projects and problem-solving, I gained experience that allows me to create efficient, reliable applications, ready to drive impact in the tech landscape.",
   },
 ];
 
 export const SOCIAL_MEDIA_LINKS = [
   {
-    href: "https://x.com/",
+    href: "https://www.facebook.com/iBoy98",
     icon: <FaFacebook fontSize={25} className="hover:opacity-80" />,
   },
   {
-    href: "https://x.com/",
-    icon: <FaDiscord fontSize={25} className="hover:opacity-80" />,
-  },
-  {
-    href: "https://x.com/",
+    href: "https://www.instagram.com/assem.salama98/",
     icon: <FaInstagram fontSize={25} className="hover:opacity-80" />,
   },
   {
-    href: "https://x.com/",
-    icon: <FaXTwitter fontSize={25} className="hover:opacity-80" />,
-  },
-  {
-    href: "https://github.com/",
+    href: "https://github.com/assem3811",
     icon: <FaGithub fontSize={25} className="hover:opacity-80" />,
   },
   {
-    href: "https://www.linkedin.com/",
+    href: "https://www.linkedin.com/in/assem-salama/",
     icon: <FaLinkedin fontSize={25} className="hover:opacity-80" />,
   },
 ];
